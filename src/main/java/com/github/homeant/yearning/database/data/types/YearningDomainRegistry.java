f6c96a072aae1f8128332da71bb1c22b419fa040
Huang Tian Hui <huangtianhui@xiaobangtouzi.com>
2021-03-03 20:07:34 +0800

    代码提交

--- src/main/java/com/github/homeant/yearning/database/data/types/YearningDomainRegistry.java
+++
@@ -1,6 +0,0 @@
-package com.github.homeant.yearning.database.data.types;
-
-import com.intellij.database.data.types.DomainRegistry;
-
-public class YearningDomainRegistry extends DomainRegistry {
-}
