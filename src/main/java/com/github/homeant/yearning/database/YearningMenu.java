3e2104b3c0a40e31400f431ecb2534cf880c2c54
Huang Tian Hui <huangtianhui@xiaobangtouzi.com>
2021-03-08 20:04:18 +0800

    插件代码提交

---
+++ src/main/java/com/github/homeant/yearning/database/YearningMenu.java
@@ -0,0 +1,12 @@
+package com.github.homeant.yearning.database;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class YearningMenu extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+
+    }
+}
