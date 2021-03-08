2e114c1b6be4cb60fb3f8fba501ab7efcf3cc7d6
homeant <homeanter@163.com>
2021-03-08 23:41:06 +0800

    代码提交

--- src/main/java/com/github/homeant/yearning/database/dialects/mysql/MysqlDialect.java
+++ src/main/java/com/github/homeant/yearning/database/dialects/mysql/MysqlDialect.java
@@ -1,44 +1,16 @@
 package com.github.homeant.yearning.database.dialects.mysql;
 
 import com.intellij.database.Dbms;
-import com.intellij.database.dialects.base.AbstractDatabaseDialect;
-import com.intellij.database.model.DasObject;
-import com.intellij.database.model.DasRoutine;
-import com.intellij.database.util.DbImplUtil;
-import com.intellij.database.util.DdlBuilder;
+import com.intellij.database.dialects.mysqlbase.MysqlBaseDialect;
 import com.intellij.openapi.util.NlsSafe;
-import com.intellij.openapi.util.Pair;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-public class MysqlDialect extends AbstractDatabaseDialect {
+public class MysqlDialect extends MysqlBaseDialect {
 
     private final Dbms dbms;
 
-    public MysqlDialect(Dbms dbms){
-        super(new MysqlTypeHelper());
+    public MysqlDialect(Dbms dbms) {
         this.dbms = dbms;
-    }
-
-
-    @Override
-    public @NotNull DdlBuilder qualifiedIdentifier(DdlBuilder ddlBuilder, @NotNull String s, @Nullable DasObject dasObject, @NotNull DasObject dasObject1) {
-        return ddlBuilder;
-    }
-
-    @Override
-    public @NotNull DdlBuilder sqlDropSequence(@NotNull DdlBuilder ddlBuilder, @NotNull DasObject dasObject, boolean b) {
-        return ddlBuilder;
-    }
-
-    @Override
-    public @Nullable Pair<String, DbImplUtil.ConcatenationProps> sqlViewDefinition(@NotNull DasObject dasObject) {
-        return null;
-    }
-
-    @Override
-    public @NotNull Pair<String, DbImplUtil.ConcatenationProps> sqlProcedureDefinition(@NotNull DasRoutine dasRoutine) {
-        return null;
     }
 
     @Override
@@ -49,25 +21,5 @@ public class MysqlDialect extends AbstractDatabaseDialect {
     @Override
     public @NlsSafe @NotNull String getDisplayName() {
         return "Yearning SQL";
-    }
-
-    @Override
-    public boolean supportsEmptyTables() {
-        return false;
-    }
-
-    @Override
-    public boolean supportsViewDefinition() {
-        return false;
-    }
-
-    @Override
-    public boolean supportsProcedureDefinition() {
-        return false;
-    }
-
-    @Override
-    public boolean supportsCommonTableExpression() {
-        return false;
     }
 }
